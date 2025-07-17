33ab0102a31029f78701ac7258d1bc31658b5d3d
techocea <dunsfordbright@gmail.com>
2025-07-17 07:13:49 +0530

    removed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
-export async function middleware(req: NextRequest) {
-  const isAdminPath = req.nextUrl.pathname.startsWith("/control-panel");
+// import { NextRequest, NextResponse } from "next/server";
+// import { jwtVerify } from "jose";
+// export async function middleware(req: NextRequest) {
+//   const isAdminPath = req.nextUrl.pathname.startsWith("/control-panel");
 
-  const adminToken = req.cookies.get("adminToken")?.value;
+//   const adminToken = req.cookies.get("adminToken")?.value;
 
-  if (isAdminPath) {
-    if (!adminToken) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
+//   if (isAdminPath) {
+//     if (!adminToken) {
+//       return NextResponse.redirect(new URL("/login", req.url));
+//     }
 
-    try {
-      await jwtVerify(
-        adminToken,
-        new TextEncoder().encode(process.env.JWT_SECRET)
-      );
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-  }
-}
+//     try {
+//       await jwtVerify(
+//         adminToken,
+//         new TextEncoder().encode(process.env.JWT_SECRET)
+//       );
+//       return NextResponse.next();
+//     } catch (error) {
+//       return NextResponse.redirect(new URL("/login", req.url));
+//     }
+//   }
+// }
 
-export const config = {
-  matcher: ["/control-panel/:path*"],
-};
+// export const config = {
+//   matcher: ["/control-panel/:path*"],
+// };
